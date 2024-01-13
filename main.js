25bc86c5df8d29717d0c2f28e2a515e3a20d1bc7
devtya1 <98102495+devtya1@users.noreply.github.com>
2024-01-13 10:25:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -33,17 +33,14 @@ function draw(){
     image(video, 0, 0, 380, 380);
     if(status != ""){
         objectDetector.detect(video, gotresult);
-        r = random(255);
-        g = random(255);
-        b = random(255);
         for(i = 0; i < objects.length; i++){
             document.getElementById("status").innerHTML = "Status : Object Detected";
             document.getElementById("no_object").innerHTML = "No of Objects: " + objects.length;
-            fill(r,g,b);
+            fill("red");
             percent = floor(objects[i].confidence *100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 20, objects[i].y + 20);
             noFill();
-            stroke(r,g,b);
+            stroke("black");
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
     }
